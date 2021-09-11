9d4120efd3b3698c032c8283ee3128db140202c9
jayesh1313 <jayeshgg2@gmail.com>
2021-09-11 12:41:18 +0530

    compeleted

--- src/components/schedular.js
+++ src/components/schedular.js
@@ -1,5 +1,6 @@
 import { AppointmentForm, Appointments, AppointmentTooltip, DayView, Scheduler } from '@devexpress/dx-react-scheduler-material-ui';
 import Paper from '@material-ui/core/Paper';
+import { ViewState, EditingState, IntegratedEditing } from '@devexpress/dx-react-scheduler';
 
 const Schedular = () => {
 
@@ -247,7 +248,36 @@ const Schedular = () => {
           location: 'Room 1',
         },
       ];
-      
+      constructor(props) {
+        super(props);
+        this.state = {
+          data: appointments,
+          currentDate: '2018-06-27',
+        };
+    
+        this.commitChanges = this.commitChanges.bind(this);
+      }
+    
+      commitChanges({ added, changed, deleted }) {
+        this.setState((state) => {
+          let { data } = state;
+          if (added) {
+            const startingAddedId = data.length > 0 ? data[data.length - 1].id + 1 : 0;
+            data = [...data, { id: startingAddedId, ...added }];
+          }
+          if (changed) {
+            data = data.map(appointment => (
+              changed[appointment.id] ? { ...appointment, ...changed[appointment.id] } : appointment));
+          }
+          if (deleted !== undefined) {
+            data = data.filter(appointment => appointment.id !== deleted);
+          }
+          return { data };
+        });
+      }
+    
+      render() {
+        const { currentDate, data } = this.state;
 
     return (
         <Paper>
@@ -258,6 +288,18 @@ const Schedular = () => {
         startDayHour={8}
         endDayHour={13}
       />
+      <ViewState
+            currentDate={currentDate}
+          />
+          <EditingState
+            onCommitChanges={this.commitChanges}
+          />
+          <IntegratedEditing />
+          <DayView
+            startDayHour={9}
+            endDayHour={19}
+          />
+          <ConfirmationDialog />
       <Appointments />
       <AppointmentTooltip
             showCloseButton
@@ -269,5 +311,6 @@ const Schedular = () => {
   </Paper>
   
     )
+}
 }
 export default Schedular;
